9bb7d5e6d7f0ea5ec7b1c2334333f29af14c0516
v-dhanapal-09 <dhanapal12309@gmail.com>
2025-09-08 10:09:21 +0530

    Add files via upload

---
+++ exe7.py
@@ -0,0 +1,7 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+age_in_5_years = age + 5
+
+print(f"Hello, {name}! You are currently {age} years old.")
+print(f"In 5 years, you will be {age_in_5_years} years old.")
